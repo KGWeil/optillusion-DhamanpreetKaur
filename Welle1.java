b70a1ba7778caa2a248f4cc4843acc54a0351e1d
Dhaman <dhaman.pabla2708@gmail.com>
2020-12-03 17:24:05 +0100

    Welle1 beendet

---
+++ Welle1.java
@@ -0,0 +1,71 @@
+
+import processing.core.PApplet;
+import processing.core.PFont;
+
+/**
+ * Klasse Welle1.
+ * Beschreibung: 
+ *
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Welle1 extends PApplet
+{       
+    int s=100;
+    /**
+     * settings() Methode 
+     * Fenstergröße size(int width, int height) und smooth(int level) muss hier eingestellt werden.
+     */
+    @Override
+    public void settings()
+    {
+        size(400,400);
+    }        
+
+    /**
+     * Die setup() Methode wird einmal aufgerufen, wenn das Programm startet.
+     * Hier werden Einstellungen wie die Hintergrundfarbe vorgenommen
+     * und Medien wie Bilder und Schriftarten geladen.
+     */
+    @Override
+    public void setup()
+    {
+       zeichneQuadrat(10,10,0, true);
+    }
+
+    /**
+     * Zeichnet ein Quadrat der optischen Illusion
+     * @param    x    x-Koordinate
+     * @param    y    y-Koordinate
+     * @param    farbe    Füllfarbe des Quadrats
+     * @param    links     Falls links true ist: Kreise links; falls links false ist: Kreise rechts
+     */
+    public void zeichneQuadrat( int x, int y, int farbe, boolean links )
+    {
+        fill(farbe);
+        rect(x,y,s,s);
+        fill(255);
+        circle(x+(1/6)*s,y+s/6,(1/6)*s);
+        circle(x+(1/6)*s,y+s-(1/6)*s,(1/6)*s);
+    }
+
+    /**
+     * Die draw() Methode wird nach der setup() Methode aufgerufen
+     * und führt den Code innerhalb ihres Blocks fortlaufend aus,
+     * bis das Programm gestoppt oder noLoop() aufgerufen wird.
+     */
+    @Override
+    public void draw()
+    {
+
+    }
+
+    /**
+     * Mit der main()-Methode wird das Programm gestartet.
+     *
+     */    
+    public static void main(String _args[]){ 
+        PApplet.main(new String[] {Welle1.class.getName() });
+    }
+
+}
